b626c27814ac0d55d753f38c0eaea1f8882249da
Otso Saarinen <saarinenotso@gmail.com>
2025-04-07 17:37:58 +0300

    fetch expenses from postgres

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,15 +1,40 @@
-import Expense from "./components/expense";
+"use client";
 
-const money = [
-    { type: "income", value: 67.25, date: "5/4/2025" },
-    { type: "expense", value: 100, date: "6/4/2025" },
-    { type: "expense", value: 83, date: "7/4/2025" },
-];
+import Expense from "./components/expense";
+import { postgres } from "./actions";
+import { useEffect, useState } from "react";
 
 export default function Expenses() {
+    const [expenses, setExpenses] = useState([
+        { type: "income", value: 67.25, date: "2025-04-05" },
+        { type: "expense", value: 100, date: "2025-04-06" },
+        { type: "expense", value: 83, date: "2025-04-07" },
+    ]);
+
+    const fetchExpenses = async () => {
+        try {
+            const data = await postgres();
+
+            // Format the data before appending
+            const formatted = data.map((item: any) => ({
+                type: item.expense_type,
+                value: item.value,
+                date: new Date(item.date).toISOString().split("T")[0], // format to YYYY-MM-DD
+            }));
+
+            setExpenses((prev) => [...prev, ...formatted]);
+        } catch (error) {
+            console.log("Error while fetching expenses: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchExpenses();
+    }, []);
+
     return (
         <>
-            {money.map((singleExpense, index) => {
+            {expenses.map((singleExpense, index) => {
                 return (
                     <Expense
                         key={index}
