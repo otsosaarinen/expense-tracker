418eccbb2e9fbcc003ea3d9714f657dae7fb007c
Otso Saarinen <saarinenotso@gmail.com>
2025-04-07 16:29:14 +0300

    add function for showcasing expenses

--- app/expenses/components/expense.tsx
+++ app/expenses/components/expense.tsx
@@ -1,3 +1,21 @@
-export default function Expense() {
-    return <></>;
+type ExpenseProps = {
+    type: string;
+    value: number;
+    date: string;
+};
+
+export default function Expense({ type, value, date }: ExpenseProps) {
+    let expenseValue = type == "income" ? `+${value}€` : `-${Math.abs(value)}€`;
+    let expenseType = type == "income" ? "text-green-500" : "text-red-500";
+
+    return (
+        <>
+            <div
+                className={`my-2 flex h-18 w-xs flex-row items-center justify-around rounded-full bg-neutral-200`}
+            >
+                <div className={`${expenseType} text-3xl`}>{expenseValue}</div>
+                <div className="text-lg">{date}</div>
+            </div>
+        </>
+    );
 }
--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,9 +1,24 @@
 import Expense from "./components/expense";
 
+const money = [
+    { type: "income", value: 67.25, date: "5/4/2025" },
+    { type: "expense", value: 100, date: "6/4/2025" },
+    { type: "expense", value: 83, date: "7/4/2025" },
+];
+
 export default function Expenses() {
     return (
         <>
-            <Expense />
+            {money.map((singleExpense, index) => {
+                return (
+                    <Expense
+                        key={index}
+                        type={singleExpense.type}
+                        value={singleExpense.value}
+                        date={singleExpense.date}
+                    />
+                );
+            })}
         </>
     );
 }
