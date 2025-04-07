bac30df0bdd3f68d58dbdbc08343aa5dd82c7961
Otso Saarinen <saarinenotso@gmail.com>
2025-04-07 15:01:21 +0300

    add dashboard page and chart component

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Chart from "./components/chart";
+
+export default function Dashboard() {
+    return (
+        <>
+            <div className="flex h-full w-full items-center justify-center">
+                <Chart />
+            </div>
+        </>
+    );
+}
