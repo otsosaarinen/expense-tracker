9fc2d876be4ff74a05ad40ffeb5f7a4a950a5d43
Otso Saarinen <saarinenotso@gmail.com>
2025-04-08 15:42:07 +0300

    add ExpenseFormat type

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -4,25 +4,20 @@ import Expense from "./components/expense";
 import { postgres } from "./pg_actions/actions";
 import { useEffect, useState } from "react";
 
+type ExpenseFormat = {
+    ID: number;
+    expense_type: string;
+    value: number;
+    date: string | Date;
+};
+
 export default function Expenses() {
-    const [expenses, setExpenses] = useState([
-        { type: "income", value: 67.25, date: "2025-04-05" },
-        { type: "expense", value: 100, date: "2025-04-06" },
-        { type: "expense", value: 83, date: "2025-04-07" },
-    ]);
+    const [expenses, setExpenses] = useState<ExpenseFormat[]>([]);
 
     const fetchExpenses = async () => {
         try {
             const data = await postgres();
-
-            // Format the data before appending
-            const formatted = data.map((item: any) => ({
-                type: item.expense_type,
-                value: item.value,
-                date: new Date(item.date).toISOString().split("T")[0], // format to YYYY-MM-DD
-            }));
-
-            setExpenses((prev) => [...prev, ...formatted]);
+            setExpenses(data);
         } catch (error) {
             console.log("Error while fetching expenses: ", error);
         }
@@ -38,9 +33,13 @@ export default function Expenses() {
                 return (
                     <Expense
                         key={index}
-                        type={singleExpense.type}
+                        type={singleExpense.expense_type}
                         value={singleExpense.value}
-                        date={singleExpense.date}
+                        date={
+                            new Date(singleExpense.date)
+                                .toISOString()
+                                .split("T")[0]
+                        }
                     />
                 );
             })}
