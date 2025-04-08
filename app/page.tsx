c13bc5b1033d0d489c5f7475f643203be36ac8a5
Otso Saarinen <saarinenotso@gmail.com>
2025-04-08 16:30:14 +0300

    add basic create account form

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,36 @@
 "use client";
 
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 export default function Home() {
     return (
         <>
-            <div className="h-full">
+            <div className="flex h-full flex-col items-center justify-center gap-2">
                 <TextField
-                    id="outlined-basic"
-                    label="Outlined"
+                    id="outlined-first-name-input"
+                    label="First Name"
                     variant="outlined"
                 />
+                <TextField
+                    id="outlined-last-name-input"
+                    label="Last Name"
+                    variant="outlined"
+                />
+
+                <TextField
+                    id="outlined-email-input"
+                    label="Email"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                />
+
+                <Button variant="contained">Create account</Button>
             </div>
         </>
     );
