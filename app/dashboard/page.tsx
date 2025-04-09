e3140e176296cc5d846fe168d2da6a5e847e7b2a
Otso Saarinen <saarinenotso@gmail.com>
2025-04-09 16:28:35 +0300

    remove title text

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,6 @@ export default function Dashboard() {
     return (
         <>
             <div className="flex w-full flex-col items-center gap-5">
-                <div className="font-medium">Dashboard</div>
                 <div className="w-7xl">
                     <Chart />
                 </div>
