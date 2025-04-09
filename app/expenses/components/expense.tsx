9d9fca03a44ef8fae1f966e57bf9fd5d8ff79ee1
Otso Saarinen <saarinenotso@gmail.com>
2025-04-09 14:16:20 +0300

    add comments

--- app/expenses/components/expense.tsx
+++ app/expenses/components/expense.tsx
@@ -5,15 +5,20 @@ type ExpenseProps = {
 };
 
 export default function Expense({ type, value, date }: ExpenseProps) {
+    // add plus or minus in front of the value
     let expenseValue = type == "income" ? `+${value}€` : `-${Math.abs(value)}€`;
-    let expenseType = type == "income" ? "text-green-500" : "text-red-500";
+
+    // make text green if type is "income", else make it red
+    let expenseStyling = type == "income" ? "text-green-500" : "text-red-500";
 
     return (
         <>
             <div
                 className={`my-2 flex h-18 w-xs flex-row items-center justify-around rounded-full bg-neutral-200`}
             >
-                <div className={`${expenseType} text-3xl`}>{expenseValue}</div>
+                <div className={`${expenseStyling} text-3xl`}>
+                    {expenseValue}
+                </div>
                 <div className="text-lg">{date}</div>
             </div>
         </>
