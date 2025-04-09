78097927ce69711b354c131d54dcf623edb2681f
Otso Saarinen <saarinenotso@gmail.com>
2025-04-09 14:16:47 +0300

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const roboto = Roboto({
     subsets: ["latin"],
 });
@@ -21,6 +26,9 @@ export default function RootLayout({
             <body
                 className={`flex h-screen flex-col ${roboto.className} antialiased`}
             >
+                <div className="flex justify-center">
+                    Welcome to expense tracker
+                </div>
                 <div className="grow">{children}</div>
             </body>
         </html>
