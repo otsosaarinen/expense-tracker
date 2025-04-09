115e3a632877c38c4ba9e656091b2ea950138bb7
Otso Saarinen <saarinenotso@gmail.com>
2025-04-09 15:41:39 +0300

    add login & register button to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,48 @@
 "use client";
 
+import { Button } from "@mui/material";
+import Link from "next/link";
+
 export default function Home() {
-    return <></>;
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center gap-10">
+                <div className="flex flex-col items-center justify-center text-center text-neutral-800">
+                    <h1 className="text-5xl">Welcome to Expense Tracker!</h1>
+                    <h2 className="text-2xl">
+                        Here you can track your daily expenses and optimize your
+                        spending
+                    </h2>
+                </div>
+                <div className="flex flex-row gap-2">
+                    <Link href="/login">
+                        <Button
+                            variant="contained"
+                            size="large"
+                            sx={{
+                                color: "success.light",
+                                backgroundColor: "white",
+                                borderColor: "success.light",
+                            }}
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                    <Link href="/registration">
+                        <Button
+                            variant="contained"
+                            size="large"
+                            sx={{
+                                color: "white",
+                                backgroundColor: "success.light",
+                                borderColor: "success.light",
+                            }}
+                        >
+                            Register
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
