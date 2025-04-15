ce233ff568ebf3733006e81d240ff2998b7be858
Otso Saarinen <saarinenotso@gmail.com>
2025-04-15 16:05:37 +0300

    add cors middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 import userAdd from "./routes/userAdd";
 import userAuth from "./routes/userAuth";
 
 const app = express();
+const cors_middleware = cors();
 const port = 4000;
 
+app.use(cors_middleware);
 app.use(express.json());
 
 app.use("/userAdd", userAdd);
