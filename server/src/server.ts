37eb7c392d9914bc01d67efee186e3ce9d554945
Otso Saarinen <saarinenotso@gmail.com>
2025-04-21 22:53:16 +0300

    add expensesAdd to API routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import userAdd from "./routes/userAdd";
 import userAuth from "./routes/userAuth";
 import expensesFetch from "./routes/expensesFetch";
+import expensesAdd from "./routes/expensesAdd";
 
 const app = express();
 const cors_middleware = cors();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/userAdd", userAdd);
 app.use("/userAuth", userAuth);
 app.use("/expensesFetch", expensesFetch);
+app.use("/expensesAdd", expensesAdd);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
